fa8b25dea28253c240eec68c97e4cc23a10e6db1
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-05-27 19:52:51 +0530

    function.dart : function? , syntax, type, categories and their examples                 function_vs_method.dart : details add

---
+++ function_vs_method.dart
@@ -0,0 +1,20 @@
+
+// function and method
+
+void main(){
+
+  // Method
+  // method is the function written inside the class
+
+
+
+  // Function
+  // function is just an individual entity
+  // function helps re-usability of code in the program
+  // function are the block of code that perform a specific task
+  // function are created when some statements are repeatedly occurring in the program
+
+  // Note : main objective of function is DRY [ Don't Repeat Yourself ]
+
+
+}
