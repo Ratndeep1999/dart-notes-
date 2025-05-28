dff956aff5c1185233f241d4f475f0caecb8c7d1
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-05-28 10:02:06 +0530

    parameters.dart : parameters?, positional, named and required in named, optional(combination of positional and named) and their e.g

--- parameters.dart
+++ parameters.dart
@@ -15,10 +15,14 @@ void main(){
   // Here, must to supply the arguments in same order as define parameters when wrote the function
   // if I call function with arguments in wrong order, I will get wrong result
 
-  // e.g
+  // e.g 1
   printData('Female','Sonali'); // passing arguments in wrong order when function calling
   printData('Sonali','Female'); // passing arguments in correct order
 
+  // e.g 2 default values on positional para.
+  printDetails('Sonali','Female'); // i forget to pass title here so, it takes default values
+  printDetails('Sonali','Female', 'miss.'); // but here i pass proper value for title
+  printDetails('Ratndeep','Male', 'mr.');
 
 
   // 2. Positional Parameter :
@@ -39,4 +43,9 @@ void main(){
 // printData function
 void printData(String name, String gender){
   print("Hello !, My name is $name and i am $gender");
+}
+
+// printDetails function
+void printDetails(String name, String gender, [String title = "mr. / miss."]){  // title is optional here if i doesn't pass title then it take default value
+  print("hello ! $title $name you are $gender");
 }
