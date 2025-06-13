f88b41bd49f2b2a7059c8002060ec2c4d97bd314
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-06-13 12:51:39 +0530

    break_and_continue.dart : break (examples), continue (examples)

--- break_and_continue.dart
+++ break_and_continue.dart
@@ -86,6 +86,11 @@ void main(){
 
 
 
+  // 2. Continue
+  // 2.1. it is allow only inside in loop
+  // 2.2. dart supports both regular and labeled continue
+  // 2.3. the continue statement skip the current iteration when it trigger and proceeds to the next one
+  // 2.4. when continue statement is encountered in a loop it doesn't terminate the loop but rather jump the flow to next iteration
 
 
   // e.g of 1 continue statement
