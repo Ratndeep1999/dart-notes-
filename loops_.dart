284851f75c4e59815674003357931aefaef34735
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-07-06 17:49:34 +0530

    loops_.dart : while, do_while, for, for_in, for_each

--- loops_.dart
+++ loops_.dart
@@ -3,7 +3,7 @@
 void main() {
 
 
-// Loop's : mostly use to execute block of code
+// Loop's : mostly use to execute block of code repeatedly
 // repeat a block of code until to meet specific condition true
 
 // note : primary purpose of all loops is to repeat a block of code
