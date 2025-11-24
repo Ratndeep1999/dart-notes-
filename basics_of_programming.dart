0cbb190a29266871ce26ae5ac2668bdd7e8f3fce
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-24 15:57:41 +0530

    Some basics concepts and terms add

---
+++ basics_of_programming.dart
@@ -0,0 +1,21 @@
+
+
+// What is Programming :
+// Process ot telling computer which task to perform, in order to solve a problem.
+
+// What is Coding :
+// Set of instructions that computer can read in binary code (0,1).
+
+// What is Statement :
+// Commands that tell computer to do something.
+
+// What is Keyword :
+// Keywords are reserved words, that gives special meaning to compiler.
+
+// What is Expression :
+// Represents value or something that can be counts as a value like number, String etc
+
+// What is Identifiers :
+// Given name's by programmer to define variables, methods and functions
+// It must be unique, not same as Keywords
+
