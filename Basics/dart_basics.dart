809051308e8743ac5d7dd4b3a11cd3148e0cb794
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-24 16:20:13 +0530

    dart basics add

---
+++ Basics/dart_basics.dart
@@ -0,0 +1,12 @@
+
+
+// Object Oriented programing language
+// Similar to java, c#, c
+// Developed by google and designed by two experts [Lars Bak and Kasper Lund] of google.
+// First revealed at GOTO conference in Denmark on Oct 10, 2011.
+// Dart 1.0 -- release -- Nov 14,2013
+// Dart 2.0 -- release -- August 2018
+// Dart 3.0 -- release -- May 2023
+
+// Use for Frond end as well as Back end.
+// Run on any platform means cross platform.
