36610ea9d77059e40eaa8a4925f3efefe912017d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-11-24 16:06:43 +0530

    more programming terms use

--- basics_of_programming.dart
+++ basics_of_programming.dart
@@ -19,3 +19,20 @@
 // Given name's by programmer to define variables, methods and functions
 // It must be unique, not same as Keywords
 
+// High-Level Programing Language :
+// Easy to learn, user-friendly and uses english like sentence like Java, Dart, c
+
+// Low Level Language :
+// Har to learn, not user-friendly and deal with computer hardware components eg. machine and assembly lang.
+
+// What is Compiler :
+// Computer program that translates high-level into machine-level language.
+
+// Syntax :
+// pattern of write programing language or rule that give concept to code.
+
+// Compile - Time :
+// Phase when we writing the code Before Execution [While converting dart code into machine code].
+
+// Run - Time :
+// Phase when program is running and executes the code [Here founded error called Run Time errors].
