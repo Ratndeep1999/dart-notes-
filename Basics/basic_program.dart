35e128090ed46c1a309f39f226e037982a5b4758
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-02 10:27:19 +0530

    basic_program.dart: last print statement change

--- Basics/basic_program.dart
+++ Basics/basic_program.dart
@@ -1,19 +1,22 @@
-
 // void main() is the entry point of any program without main() compiler return runtime exception.
-void main() {  // here void means this function doesn't returning any value, 'void' can be avoid.
+void main() {
+  // here void means this function doesn't returning any value, 'void' can be avoid.
 
-  String fullName = "Ratndeep Chandankhede"; // (semi-colon) ; tells compiler to terminates code statement.
-  var firstName = "Ratndeep" ;
-  var lastName = "Chandankhede" ;
+  String fullName =
+      "Ratndeep Chandankhede"; // (semi-colon) ; tells compiler to terminates code statement.
+  var firstName = "Ratndeep";
+  var lastName = "Chandankhede";
   int age = 26;
 
   // print() is pre-defined function use to print values that stored inside variable or entity.
-  print(fullName);  // Ratndeep Chandankhede
+  print(fullName); // Ratndeep Chandankhede
 
   // String interpolation
   print("My name is $firstName and surname is $lastName");
 
   // string concatenation
-  print("My full name is ${firstName + " " + lastName}"); // My full name is Ratndeep Chandankhede
-
-} // curly braces {} represents beginning and ending of the block of code
+  print(
+    "My full name is ${firstName + " " + lastName} and i am years $age old",
+  ); // My full name is Ratndeep Chandankhede and i am years 26 old
+}
+// curly braces {} represents beginning and ending of the block of code
