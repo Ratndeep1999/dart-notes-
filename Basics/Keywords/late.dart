4a0f070e1f3aed6b69678f39177615ddf3876cdd
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-02 21:20:42 +0530

    late.dart: description and example add

---
+++ Basics/Keywords/late.dart
@@ -0,0 +1,11 @@
+
+
+void main(){
+  /// late
+  // It ensure that the variable is initialize before use
+  // we can assign value before use
+
+  late double amount;
+  amount = 25.55;
+  print(amount);  // 25.55
+}
