2873bc88743f027ec66950fd2115597697640f3c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-02 10:12:18 +0530

    features_of_dart.dart : features add

---
+++ Basics/Before Basics/features_of_dart.dart
@@ -0,0 +1,15 @@
+
+/// Features of Dart
+
+// Free and open source programing lang.
+// Object oriented and supports all oops features.
+// Have multi (cross) platform deployment features.
+// Single threaded but supports asynchronous.
+// Has type safety and null safety
+// Supports two type of compilation precess AOT (ahead of time) & JIT (just in time).
+// Ever single component of dart we can create is an object
+// Use for both front end and server end.
+// Easy to learn and Fast Development
+// Garbage collection [Automatic memory management]
+// Optionally typed Language
+// Strongly typed language
