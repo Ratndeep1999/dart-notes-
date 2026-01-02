b1e1845aea934480f01b0efa327854b9b8c87b27
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-02 10:08:39 +0530

    basics_of_programming.dart: programing basics add

---
+++ Basics/Before Basics/basics_of_programming.dart
@@ -0,0 +1,57 @@
+/// Basics of Programing
+
+/// What is Programming :
+// Process ot telling computer which task to perform, in order to solve a problem.
+
+/// What is Coding :
+// Set of instructions that computer can read in binary code (0,1).
+
+/// What is Statement :
+// Commands that tell computer to do something.
+
+/// What is Keyword :
+// Keywords are reserved words, that gives special meaning to compiler.
+
+/// What is Expression :
+// Represents value or something that can be counts as a value like number, String etc
+
+/// What is Identifiers :
+// Given name's by programmer to define variables, methods and functions
+// It must be unique, not same as Keywords
+
+/// High-Level Programing Language :
+// Easy to learn, user-friendly and uses english like sentence like Java, Dart, c
+
+/// Low Level Language :
+// Har to learn, not user-friendly and deal with computer hardware components eg. machine and assembly lang.
+
+/// What is Compiler :
+// Computer program that translates high-level into machine-level language.
+
+/// Syntax :
+// pattern of write programing language or rule that give concept to code.
+
+/// Compile - Time :
+// Phase when we writing the code Before Execution [While converting dart code into machine code].
+
+/// Run - Time :
+// Phase when program is running and executes the code [Here founded error called Run Time errors].
+
+/// Difference Between Compile & Run Time:
+// Compile-time : phase when dart check the codes before execution.
+// type and syntax error occur at compile-time called as compile time error.
+
+// Run-time : phase when the program executing.
+// logical and exceptions occur at run-time known as run-time error.
+
+/// Scope
+// it defines the accessible area in the program.
+// There are two types of Scopes Local and Global.
+
+/// Local Scope :
+// variable that define inside particular function or block of the code, and is accessible only
+// inside that function or block of code is known as Local Scope.
+// Scope where variable can be define indide function or block of code is only accessible inside that area.
+
+/// Global Scope :
+// variable that define outside any function or block is accessible anywhere in the program or project.
