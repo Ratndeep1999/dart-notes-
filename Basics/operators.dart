e0e1eae7c17d446e952963a4bc549009652d6021
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-02 10:54:24 +0530

    operators.dart : basic operators update

--- Basics/operators.dart
+++ Basics/operators.dart
@@ -8,97 +8,96 @@
 ///Types
 
 void main() {
-  /// Arithmetic Operator
-  int num1 = 10;
-  int num2 = 2;
+  /// 1. Arithmetic operator
+  int a = 10, b = 4, c = 3;
 
-  print("addition: ${num1 + num2}"); // 12
-  print("substraction: ${num1 - num2}"); // 8
-  print("multiplication: ${num1 * num2}"); // 20
-  print("division: ${num1 / num2}"); // 5.0
-  print("modulus(remainder): ${num1 % num2}"); // 0
-  print("modulus(remainder): ${num2 % num1}"); // 2
-  print("unary minus: ${-num1}"); // -10
-  print("integer division: ${num1 ~/ num2}"); // 5
-  print("integer division: ${num2 ~/ num1}"); // 0
+  print(a + b); // addition 14
+  print(a - b); // subtraction 6
+  print(a * b); // multiplication 40
+  print(a / b); // division 2.5
+  print(a ~/ b); // integer division  2
+  print(a % c); // remainder (modulo) 1
 
-  /// Assignment Operator
+  /// 2. Assignment Operator
   num age = 26;
   print("age: ${age += 1}"); // age = age + 1  // age: 27
   print("age: ${age -= 2}"); // age = 27 - 2   // age: 25
   print("age: ${age *= 2}"); // age = 25 * 2   // age: 50
   print("age: ${age /= 2}"); // age = 50 / 2  // age: 25.0
 
-  /// Relational Operator
-  int num3 = 5;
-  int num4 = 10;
-  int num5 = 10;
+  /// 3. Relational operator
+  print('10 < 20 : ${10 < 20}'); // true  less-than
+  print('10 <= 5 : ${10 <= 5}'); // false  less-than equal to
+  print('10 > 5 : ${10 > 5}'); // true  greater than
+  print('10 >= 5 : ${10 >= 10}'); //  true   greater than equal to
+  print('10 == 5 : ${10 == 5}'); // false  equality test
+  print('10 != 5 : ${10 != 10}'); // false  not equal to
 
-  // less than
-  print(num3 < num4); // true
-  // less than or equal to
-  print(num4 <= num3); // false
-  // greater than
-  print(num3 > num4); // false
-  // greater than or equal to
-  print(num4 >= num5); // true
-  // equal to
-  print(num4 == num5); // true
-  // not equal to
-  print(num4 != num5); // false
+  /// 4. Logical operator
 
-  print("\n"); // next line
+  // 4.1. && logical AND operator  (True if both condition is true)
+  print(10 > 5 && 10 < 20); // both are true : true
+  print(10 < 5 && 10 < 20); // first one is false : false
 
-  /// Logical Operator
-  // && (And) : if both condition true then return 'true' otherwise 'false'
-  print((num3 == num4) && true); // false
-  print(true && (num5 >= num3)); // true
+  // 4.2. || logical OR operator  (True if at least any one condition is true)
+  print(10 > 5 || 10 > 20); // first one is true only : true
+  print(10 <= 5 || 10 >= 20); // both are false : false
 
-  // || (Or) : if any one condition true then return 'true' otherwise 'false'
-  print((num3 >= num4) || (num4 != num5)); // false
-  print(false || (num4 == num5)); // true
+  // 4.3. ! (Not) it Toggles true to false means voice verse
+  var u = false;
+  var v = !u;
+  print(v); // true
 
-  // ! (Not) : voice verse of value
-  bool isMarried = false;
-  print(isMarried); // false
-  print(!(num4 != num5)); // true
-
-  /// Conditional / Ternery Operator
+  /// 5. Conditional / Ternery Operator
   // condition ? (execute this if true) : (execute this if false)
-  print("Ratndeep are you married ? $isMarried!",); // Ratndeep are you married ? false!
+
+  late bool result  ;
+  result = (10 > 5) ? true : false ;   // this returning bool value
+  print('result : $result');  // result : true
+
+  bool isMarried = false;
+  print("Ratndeep are you married ? $isMarried!"); // Ratndeep are you married ? false!
   String condition = isMarried ? "Yes" : "No";
   print("Ratndeep are you married ? $condition!",); // Ratndeep are you married ? No!
 
-  /// Increment and Decrement operator
-  // pre increment / decrement
-  int ratndeepAge = 26;
 
-  // pre increment : increase by 1
-  int myAgeIn2026 = ++ratndeepAge;
-  print("My Age in Year 2026 is $myAgeIn2026"); // My Age in Year 2026 27
+  /// 6. Increment and Decrement operator
+  // 6.1.  post inc and dec
+  int num1 = 10 ;
 
-  // pre decrement : decrease by 1
-  myAgeIn2026 = --myAgeIn2026;
-  print("My Age in Year 2025 $myAgeIn2026"); // My Age in Year 2025 26
+  // post increment
+  int num2 = num1++ ;
+  print('num1 = $num1'); // 11
+  print('num2 = $num2'); // 10
 
-  // post increment / decrement
-  myAgeIn2026 = 27;
+  // post decrement
+  num1 = 10;
+  num2 = num1-- ;
+  print('num1 = $num1');  //  9
+  print('num2 = $num2');  //  10
 
-  // post increment : no increase
-  myAgeIn2026 = myAgeIn2026++;
-  print("My Age in Year 2026 $myAgeIn2026"); // My Age in Year 2026 27
 
-  // post decrement : no decrease
-  myAgeIn2026 = myAgeIn2026--;
-  print("My Age in Year 2026 $myAgeIn2026"); // My Age in Year 2026 27
+  // 6.2.  pre inc and dec
+  num1 = 10 ;
 
-  /// Type Test Operator
+  // pre increment
+  num2 = ++num1;
+  print('num1 = $num1');  // 11
+  print('num2 = $num2');  // 11
+
+  num1 = 10;
+
+  // pre decrement
+  num2 = --num1;
+  print('num1 = $num1');  //  9
+  print('num2 = $num2');  //  9
+
+  /// 7. Type Test Operator
   // use to check runtime type and return boolean value
   String text = "This is String";
 
   // is
-  print(text is String);  // true
+  print(text is String); // true
   // is!
-  print(text is! String);  // false
-
+  print(text is! String); // false
 }
