1ea29be8d7d08fa82eab1b147be2d3b7a59fac4d
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-03 21:40:21 +0530

    for_loop.dart: loops?, for?, description, syntax, and example.

---
+++ Loops/for_loop.dart
@@ -0,0 +1,28 @@
+
+
+/// Loops
+// It is use to execute a block of code multiple times until
+// certain conditions are not not meet
+// Main purpose is to repeat the block of code without writing it multiple time.
+
+/// Types
+// For loop
+// While loop
+// Do while
+// For in loop
+
+
+/// for
+// Use when we know, how many times the code need to repeat.
+
+/// syntax
+// for ( initialization; condition ; post increment) {
+//   statement
+// }
+
+void main(){
+  for (int i = 1; i <= 10; i++){
+    print("i = $i");
+  }
+}
+
