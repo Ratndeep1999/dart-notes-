5c52781925caad7667d84c9a32e125abacb3249b
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-04 11:13:54 +0530

    break.dart: description, break with for, while, and do while.

---
+++ Jump Statements/break.dart
@@ -0,0 +1,50 @@
+/// break
+// Use to exit from loop, it stops loop immediately and the program's
+// control moves outside the loop.
+// Use to terminate the execution of loops as well as switch case.
+// Outside the loop's and the switch case, break is not applicable.
+
+void main() {
+  // break with for loop
+  for (int i = 1; i <= 10; i++) {
+    if (i == 4) {
+      break;
+    }
+    print('i = $i');
+  }
+
+  // i = 1
+  // i = 2
+  // i = 3
+
+  // break with while loop
+  int j = 10;
+  while (j >= 1) {
+    if (j == 4) {
+      break;
+    }
+    print('j = $j');
+    j--;
+  }
+  // j = 10
+  // j = 9
+  // j = 8
+  // j = 7
+  // j = 6
+  // j = 5
+
+  // break with do while loop
+  int b = 10;
+  do {
+    if (b == 5) {
+      break;
+    }
+    print('b = $b');
+    b--;
+  } while (b >= 1);
+  // b = 10
+  // b = 9
+  // b = 8
+  // b = 7
+  // b = 6
+}
