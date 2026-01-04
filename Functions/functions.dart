08c87c115f8a9924290962b31df9e389b1b26d59
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-04 18:46:03 +0530

    functions.dart: Function?, key points, advantages, diff between function and method, syntax, and components of function.

---
+++ Functions/functions.dart
@@ -0,0 +1,44 @@
+
+
+/// Functions
+// It is a block of the code to perform that use to perform specific task.
+
+/// DRY
+// Don't repeat yourself (main objective of any function)
+
+/// Key Points
+// 1. Use to divide large part of code into small part.
+// 2. Use to perform repetitive operation in small part of code
+//    like re-usability of repetitive component.
+// 3. It gives readability, and helps to make program simple and maintainable.
+// 4. In dart, function is an object, it can be assign to variable, pass
+//    as a parameter to another function ans can be used as returned value as well.
+
+/// Advantages
+// Helps to reduce code and give readability.
+// Helps to avoid code repetition.
+// Helps to divide complex program into small part for readability.
+
+/// Difference between Function and Method
+// Method():
+// When a function written inside class, where function is just an individual entity.
+
+// Function():
+// It is a block of code that perform a specific task, basically function
+// created outside any class.
+
+/// Syntax
+// returnType functionName(parameters){
+//   body of the function
+//   return value;
+// }
+
+/// Components of function
+// 1. return type : It tell's, what the type of value that function returns.
+// If function doesn't return any value then i can use void as return type.
+
+// 2. function name : It is define after function type, it always follow lowerCamelCase.
+
+// 3. parameters : Parameters are inputs of the function, that wright inside parenthesis().
+
+// 4. return : It is keyword use to return calculated value from the function.
