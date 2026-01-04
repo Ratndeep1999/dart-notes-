268d50a3089425c28d2734d785b3137181fac95c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-04 08:17:29 +0530

    for_loop.dart: loops?, for?, description, syntax, and example.

--- Loops/for_loop.dart
+++ Loops/for_loop.dart
@@ -15,14 +15,67 @@
 /// for
 // Use when we know, how many times the code need to repeat.
 
+/// Execution Flow
+// first : check initialization (once)
+// second : check condition
+// third : if condition is true then block of code execute
+// fourth : execute increment or decrement after the block of code execute
+// fifth : check condition again
+// sixth : if condition is true then block of code execute
+
+/// It is also known as exit controller loop
+
 /// syntax
-// for ( initialization; condition ; post increment) {
-//   statement
+// for ( initialization; condition ; inc/dec) {
+//   statements;
 // }
 
 void main(){
   for (int i = 1; i <= 10; i++){
     print("i = $i");
   }
+
+  // i = 1
+  // i = 2
+  // i = 3
+  // i = 4
+  // i = 5
+  // i = 6
+  // i = 7
+  // i = 8
+  // i = 9
+  // i = 10
+
+  int j = 10;
+  for ( j; j >= 1; j--){
+    print("j = $j");
+  }
+
+  // j = 10
+  // j = 9
+  // j = 8
+  // j = 7
+  // j = 6
+  // j = 5
+  // j = 4
+  // j = 3
+  // j = 2
+  // j = 1
+
+  /// infinite loop
+  // for (int a = 1; a >= 1; a++){
+  //   print('a : $a');
+  // }
+
+  // a : 1
+  // a : 2
+  // a : 3
+  // a : 4
+  // a : 5
+  // a : 6
+  // .......infinite
+
+
+
 }
 
