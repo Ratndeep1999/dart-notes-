92ca4ea4207d213ddcf1afdcbb5a4afcd35444c0
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-05 10:25:52 +0530

    positional.dart: definition, function definition and example add.

---
+++ Functions/Parameters/positional.dart
@@ -0,0 +1,16 @@
+/// Positional Parameters
+// Here, position/order of parameters matter.
+// arguments position must match with the positional parameter when function invoke.
+
+// Function Definition
+myBioData(String name, int age, int height){
+  print('My self $name, my age is $age and my height is $height cm');
+}
+
+void main(){
+  // arguments position is wrong
+  myBioData('Ratndeep', 166, 26); // My self Ratndeep, my age is 166 and my height is 26 cm
+
+  // arguments position match with parameters position
+  myBioData('Ratndeep', 26, 166); // My self Ratndeep, my age is 26 and my height is 166 cm
+}
