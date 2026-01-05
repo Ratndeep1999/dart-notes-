5fc6302481257a709f4c4b30b7c991cfe7f154da
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-05 09:42:46 +0530

    type_of_function.dart: pre-defined and user-defined function with their examples.

---
+++ Functions/type_of_function.dart
@@ -0,0 +1,22 @@
+/// Types of Functions
+
+/// Pre defined Function
+// Functions that already build in the dart library, they are just ready to use
+// e.g. print() and main() both are pre defined
+
+/// User defined Function
+// Function that create by the developers to perform specific task
+// e.g. add() is user defined function
+
+void main() {
+  String name = 'Ratndeep';
+  print('name'); // print() - pre defined
+
+  print('Sum is ${add(10, 15)}'); // Sum is 15
+}
+
+// Function definition
+int add(int num1, int num2) {
+  int sum;
+  return sum = num1 = num2;
+}
