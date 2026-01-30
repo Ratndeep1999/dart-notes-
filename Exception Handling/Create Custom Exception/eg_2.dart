ab0fb69842f0784979822c910d3de6982b2250ad
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-30 14:14:09 +0530

    eg_.dart: second example of custom exception class.

---
+++ Exception Handling/Create Custom Exception/eg_2.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+/// Custom Exception class
+class NegativeSquareRootException implements Exception {
+  /// object of NegativeSquareRootException class
+  @override
+  String toString() {
+    return "Square root of negative number is not allowed here";
+  }
+}
+
+/// method to get square root of positive number
+num squareRoot(int number) {
+  if (number < 0) {
+    throw NegativeSquareRootException();
+  } else {
+    return sqrt(number);
+  }
+}
+
+void main() {
+  /// try-catch-on-finally
+  try {
+    var result = squareRoot(-4);
+    print(result);
+  } on NegativeSquareRootException catch (e) {
+    print("Oops, Negative Number: $e");
+  } catch (e) {
+    print(e);
+  } finally {
+    print("Job Completed!");
+  }
+
+  // Oops, Negative Number: Square root of negative number is not allowed here
+  // Job Completed!
+}
