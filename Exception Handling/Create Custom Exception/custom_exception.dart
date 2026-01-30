7e6ba9b4069422485220c69471737858ff4a9772
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-01-30 14:12:45 +0530

    custom_exception.dart: what is custom exception? and syntax.

---
+++ Exception Handling/Create Custom Exception/custom_exception.dart
@@ -0,0 +1,15 @@
+/// Custom Exception
+// A custom exception is a user-defined class that represents
+// application-specific errors.
+// It implements the Exception interface and allows us to throw
+// meaningful and readable error messages.
+
+/// syntax
+// class YourException implements Exception {
+//   final String message;
+//
+//   YourException(this.message);
+//
+//   @override
+//   String toString() => message;
+// }
