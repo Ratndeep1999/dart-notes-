1c96d84c14bf2744644da331d9cd6f16833535bc
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-04 15:38:16 +0530

    parameters_and_arguments.dart: definition change to short.

--- Functions/parameters_and_arguments.dart
+++ Functions/parameters_and_arguments.dart
@@ -1,35 +1,32 @@
-
-
 /// Difference between Parameters and Arguments
 
-// Function Definition
-// 'add' Function with no return type
-void add(int num1, int num2){  // parameters
-  int sum =  num1 + num2;
+// add() function with no return type
+void add(int num1, int num2) {
+  // parameters
+  int sum = num1 + num2;
   print('Sum : $sum');
 }
 
 // remove Function with return type
-int remove(int num1, int num2){
+int remove(int num1, int num2) {
+  // parameters
   int result = num1 - num2;
   return result;
 }
 
-// main() Function
-void main(){
-  // Function call or invoke
-  add(10, 20);  // arguments
+void main() {
+  // Function call / invocation
+  add(10, 20); // arguments
 
-  //
   int number = remove(20, 5);
   print('Number : $number');
 }
 
 /// Parameters
-// Variables defined in the function definition.
-// variable with data-type that defined as an input for the function
-// Here, functionName(int num1, int num2), so num1 and num2 both are parameters.
+// Parameters are variables defined in the function definition. They receive
+// values when the function is called.
+// Example: num1 and num2 in add(int num1, int num2)
 
 /// Arguments
-// It is Values that passed when the function called.
-// Here, add(10, 20) and remove(20, 5) - 10, 20, 20, and 5 is arguments.
+// Arguments are the actual values passed to the function during function call.
+// Example: 10, 20, 20, and 5
