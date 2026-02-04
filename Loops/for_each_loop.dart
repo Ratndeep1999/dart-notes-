7dff019a68de0f8e4ea2a4be951461fcceec3533
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-04 14:17:51 +0530

    for_each_loop.dart: minor definition change.

--- Loops/for_each_loop.dart
+++ Loops/for_each_loop.dart
@@ -1,6 +1,6 @@
 /// for each
-// Use to iterates in the collection like list, set, or map
-// to iterates the values of collection we use forEach loop
+// Used to iterate over collections such as List, Set, and Map. It executes a function once
+// for each element in the collection. forEach is a function provided by dart collection.
 
 /// syntax
 // collection.forEach(void function(element) {
@@ -9,12 +9,11 @@
 
 /// Higher-Order Function
 // A higher-order function is a function that takes another function as a parameter or returns a function.
-// In this case, .forEach() is a higher-order function because it accepts a function as an argument.
+// forEach() is a higher-order function because it accepts a function as an argument.
 
 /// Callback function
-// A callback function is the function you pass into another function to be "called back" later during execution.
-// Here, the function(value) { ... } part is the callback function.
-// .forEach() will call this callback once for each element in the collection.
+// A callback function is the function passed to another function to be (called back) executed later.
+// In forEach(), the function inside parentheses is the callback.
 
 void main() {
   // List Collection
