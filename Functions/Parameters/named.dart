77bb072d6fb28a94befb1924d04b5175917620e2
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-02-04 16:10:20 +0530

    named.dart: definition and note change.

--- Functions/Parameters/named.dart
+++ Functions/Parameters/named.dart
@@ -1,7 +1,11 @@
 /// Named Parameters
-// Here, parameters define inside curly braces and order doesn't matter.
-// Use to mention parameters meaning (hint) at time of function invoke.
-// Named parameters is optional so must to give '?' or 'required'.
+// Named parameters are defined inside curly braces { }.
+// The order of arguments does NOT matter. They improve readability by providing
+// parameter names at the time of function call.
+
+/// Note
+// Named parameters are optional by default. use  `required` to make them mandatory,
+// or Use `?` only when a parameter can accept null.
 
 // student Function Definition
 student({required String name, required int age, required int rollNo}) {
@@ -17,7 +21,7 @@ void main() {
   // Student name : Ratndeep
   // age : 26
   // rollNo. : 1
-  student(name: 'Sonali', age: 28, rollNo: 2);
+  student(name: 'Sonali', rollNo: 2, age: 28);
   // Student name : Sonali
   // age : 28
   // rollNo. : 2
